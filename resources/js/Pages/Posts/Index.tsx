f84145c062aa361497ba0adffece78668fa3b03a
Naoki Haba <naosprintrunner6385@gmail.com>
2024-06-14 17:22:43 +0900

    投稿フォームをダッシュボードに追加し、投稿の送信機能を実装

--- resources/js/Pages/Posts/Index.tsx
+++ resources/js/Pages/Posts/Index.tsx
@@ -1,9 +1,24 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import {Head} from '@inertiajs/react';
-import {PageProps, PostsProps} from '@/types';
+import {Head, useForm} from '@inertiajs/react';
+import {PageProps, Post, PostsProps} from '@/types';
 
 
 export default function Dashboard({auth, posts}: PageProps<PostsProps>) {
+    const {data, setData, post, processing, errors, reset, clearErrors} = useForm({
+        body: '',
+    })
+
+    const submit = (e: any) => {
+        e.preventDefault()
+        post(route('posts.store'), {
+            onSuccess: () => {
+                reset('body')
+            }
+
+        })
+    }
+
+
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -14,14 +29,44 @@ export default function Dashboard({auth, posts}: PageProps<PostsProps>) {
             </Head>
 
             <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        {posts?.map((post) => (
-                            <div key={post.id} className="p-6 text-gray-900">
-                                <div className="text-lg font-semibold">{post.body}</div>
+                <div className="max-w-3xl mx-auto sm:px-6 lg:px-8 space-y-3">
+                    <form className="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6" onSubmit={submit}>
+                        <label form="body" className="sr-only">Body</label>
+                        <textarea
+                            name="body"
+                            id="body"
+                            rows={5}
+                            cols={30}
+                            className="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full"
+                            onChange={(e) => setData('body', e.target.value)}
+                            value={data.body}
+                            onFocus={() => clearErrors('body')}
+                        >
+                    </textarea>
+                        {errors.body && <div className="text-red-500">{errors.body}</div>}
+
+                        <button type="submit"
+                                className="mt-2 bg-gray-700 px-4 py-2 rounded-md font-medium text-white ${processing ? 'opacity-50' : ''}"
+                                disabled={processing}
+                        >
+                            Post
+                        </button>
+                    </form>
+
+                    {posts.data.map((post: Post) => {
+                        return (
+                            <div className=" bg-white overflow-hidden shadow-sm sm:rounded-lg" key={post.id}>
+                                <div className="p-6 text-gray-900">
+                                    <div className="font-semibold">
+                                        {post.user.name}
+                                    </div>
+                                    <p className="mt-1">
+                                        {post.body}
+                                    </p>
+                                </div>
                             </div>
-                        ))}
-                    </div>
+                        )
+                    })}
                 </div>
             </div>
         </AuthenticatedLayout>
