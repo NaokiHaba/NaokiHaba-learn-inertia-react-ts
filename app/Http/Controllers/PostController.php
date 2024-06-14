faf55e853eeaa1911756400a5844932f714bc2cf
Naoki Haba <naosprintrunner6385@gmail.com>
2024-06-14 17:22:22 +0900

    "PostControllerに新機能を追加し、コードを更新"

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,16 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StorePostRequest;
+use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Inertia\Inertia;
 
 class PostController extends Controller
 {
-    public function index()
+    public function index(): \Inertia\Response
     {
-        $posts = Post::all();
+        $posts = Post::with('user')->latest()->get();
+
         return Inertia::render('Posts/Index', [
-            'posts' => $posts
+            'posts' => PostResource::collection($posts)
         ]);
     }
+
+    public function Store(StorePostRequest $request)
+    {
+        auth()->user()->posts()->create($request->validated());
+    }
+
 }
